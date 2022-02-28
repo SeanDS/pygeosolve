eba408ece74f0a45e272e631cb15464cb1d967a0
Sean Leavey <sean.leavey@aei.mpg.de>
2022-02-28 15:23:55 +0100

    Fix setuptools_scm

--- pygeosolve/__init__.py
+++ pygeosolve/__init__.py
@@ -4,7 +4,7 @@
 try:
     from ._version import version as __version__
 except ImportError:
-    raise FileNotFoundError("Could not find version.py. Ensure you have run setup.")
+    raise FileNotFoundError("Could not find _version.py. Ensure you have run setup.")
 
 
 from .problem import Problem
